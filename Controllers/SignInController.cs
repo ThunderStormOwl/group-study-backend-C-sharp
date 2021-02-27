ddc7a75f949e023dfbf3b670cff64387d03a21cb
ThunderStormOwl <lausrock@gmail.com>
2021-02-27 11:44:43 -0300

    SignIn and SignUp controllers set up

---
+++ Controllers/SignInController.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignInController : ControllerBase
+    {
+        private readonly BackendContext _context;
+
+        public SignInController(BackendContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/SignIn
+        [HttpPost]
+        public async Task<ActionResult<User>> SignIn([FromHeader] string email, [FromHeader] string password)
+        {
+            try{
+
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+
+                if(user != null){
+
+                    return Ok( new {accesToken = "123456789"});
+                }
+                else
+                    return BadRequest("Email or password not registered");
+                
+            }
+            catch (System.Exception){
+                return BadRequest("Something went wrong when logging in!");
+            }
+           
+        }
+    }
+}
---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        private readonly BackendContext _context;
+
+        public SignUpController(BackendContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/SignIn
+        [HttpPost]
+        public async Task<ActionResult<User>> SignUp([FromBody] User newUser)
+        {
+            try{
+
+                var duplicatedUser = await _context.Users.FirstOrDefaultAsync(u => u.Email == newUser.Email);
+
+                if(duplicatedUser == null){
+
+                    _context.Users.Add(newUser);
+                    await _context.SaveChangesAsync();
+
+                     return CreatedAtAction("SignUp", new {Id = newUser.Id}, new 
+                        { 
+                            userName = newUser.UserName,
+                            email = newUser.Email,
+                            name = newUser.Name
+                        });
+                }
+                else
+                    return BadRequest("Email already registered!");
+                
+            }
+            catch (System.Exception){
+                return BadRequest("Something went wrong when creating user!");
+            }
+           
+        }
+    }
+}
